e02fc8d2b647f388d557971aa731b396943d67f5
Philip Rhinow <Philipprhinow@gmail.com>
2026-05-14 19:54:03 +0200

    spotify api folgen gatherer

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ client_credentials_manager = SpotifyClientCredentials(client_id=client_id, clien
 
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+
 print("Success")
 print(sys.version)
 print("==================================")
