534fdb55421c2d1e42f1c7cb7f67967d7b69ffd2
Philip Rhinow <Philipprhinow@gmail.com>
2026-05-14 20:25:20 +0200

    spotify api folgen gatherer

--- test.py
+++ test.py
@@ -64,41 +64,7 @@ for i in range(0,20):
         print("created file")
 
 
-"""
-for i in range(1, 10):
-    search_folgen_name = "00" + str(i) + "/und"
-    album = sp.search(q= search_folgen_name,type="album", limit=1)
-    folgen_name = album["albums"]["items"][0]["name"]
-    print("name = " + folgen_name)
-    album_id = album["albums"]["items"][0]["id"]
-    print("id = " + album_id)
-    album = sp.album(album_id)
-    album_cover = album["images"][1]["url"]
-    print("cover link = " + album_cover)
 
-    ordner = "C:\\Users\\Anwender\\Documents\\Obsidian Vault\\Obsidian Vault\\Die drei Fragezeichen"
-
-
-    url = requests.get(album_cover).content
-
-    file_name = os.path.join(ordner + "\\Folgen_cover", folgen_name.replace("/", "_") + ".jpg")
-    with open(file_name, "wb") as f:
-        f.write(url)
-    print("gespeichert")
-
-    time.sleep(0.5)
-
-    release_date = album["release_date"]
-    cover_path = ordner + "\\Folgen_cover\\" + folgen_name
-    properties = "---\n" + "release_date: " + release_date + "\n" + "cover: \"[[" + folgen_name.replace("/", "_") + ".jpg]]\"\n---"
-
-
-    file_path = ordner +"\\Folgen\\" + folgen_name.replace("/", "_") + ".md"
-
-    with open(file_path, "w") as file:
-        file.write(properties)
-        print("created file")
-"""
 
 
 
